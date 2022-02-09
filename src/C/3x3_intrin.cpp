76e51d6bf0b05b2d9c8c2d23f828729c5a321edf
Nathan <natkeo1@gmail.com>
2022-02-09 09:25:40 -0500

    Added SSE Comparison Instructions

--- src/C/3x3_intrin.cpp
+++ src/C/3x3_intrin.cpp
@@ -12,26 +12,25 @@ void print(const std::vector<unsigned short int>& v)
     std::cout << std::endl;
 }
 
+
 bool check(const std::vector<unsigned short int>& v){
 
     __m128i t1 = _mm_set_epi16(v[0],v[0],v[0],v[1],v[2],v[2],v[3],v[6]);
     __m128i t2 = _mm_set_epi16(v[1],v[3],v[4],v[4],v[5],v[4],v[4],v[7]);
     __m128i t3 = _mm_set_epi16(v[2],v[6],v[8],v[7],v[8],v[6],v[5],v[8]);
-
+    __m128i r2 = _mm_set_epi16(15, 15, 15, 15, 15, 15, 15, 15);
+    
     __m128i r1 = _mm_add_epi16(t1,t2);
     r1 = _mm_add_epi16(r1,t3);
-
-    unsigned short int b[8];
-    _mm_store_si128((__m128i *)&b[0],r1);
-
-    for (int i;i<8;i++){
-        if(b[i] != 15){
-            return 0;
-        }
+    
+    if(_mm_test_all_ones(_mm_cmpeq_epi8(r1,r2))) {
+        return 1;
     }
-
-    return 1;
+    else{
+        return 0;
+    }
 }
+
 
 
 int main(){
