bd3a41775dc49c1edf25d27d83cdbeef863ecf40
Nathan <natkeo1@gmail.com>
2022-04-25 09:12:33 -0400

    Find a Specific Permutation

--- src/C/3x3_intrin.cpp
+++ src/C/3x3_intrin.cpp
@@ -31,15 +31,61 @@ bool check(const std::vector<unsigned short int> &v){
     }
 }
 
+std::vector<unsigned short int> findPermutation(int n, int k){
+    /*
+    Based on:
+    https://stackoverflow.com/questions/31216097/given-n-and-k-return-the-kth-permutation-sequence
+    */
+
+    int *numbers = new int[n];
+    int *indices = new int[n];
+
+    // initialise the numbers 1, 2, 3...
+    for (int i = 0; i < n; i++)
+        numbers[i] = i + 1;
+
+    int divisor = 1;
+    for (int place = 1; place <= n; place++){
+        if((k / divisor) == 0)
+            break;  // all the remaining indices will be zero
+
+        // compute the index at that place:
+        indices[n-place] = (k / divisor) % place;
+        divisor *= place;
+    }
+
+    // permute the numbers array according to the indices:
+    for (int i = 0; i < n; i++){
+        int index = indices[i] + i;
+
+        // take the element at index and place it at i, moving the rest up
+        if(index != i){
+            int temp = numbers[index];
+            for(int j = index; j > i; j--)
+               numbers[j] = numbers[j-1];
+            numbers[i] = temp;
+        }
+    }
+    std::vector<unsigned short int> r;
+    // print out the permutation:
+    for (int i=0; i<n;i++){
+       r.push_back(numbers[i]);
+    }
+    return r;
+}
+
 int main(){
     std::vector<unsigned short int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     
     int x = 0;
-    while (std::next_permutation(v.begin(), v.end())){
-        x++;
+    int c = 163440;
+    while (x!=c && std::next_permutation(v.begin(), v.end())){
+        int i = 362880 - x++;
         if (check(v)){
             std::cout << x << ":" << std::endl;
             print(v);
+            std::cout << i << ":" << std::endl;
+            print(findPermutation(9, i-1));
         }
     }
     return 0;
